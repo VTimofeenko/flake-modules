ec8036712ed93ee7cfd53d2545bcd528aa73e490
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-20 17:57:59 -0800

    break: remove self-check
    Looks like flake-parts does not pass self to perSystem attributes. The check can be restored if self is pulled from outer scope

--- flake-modules/bumpInputs.nix
+++ flake-modules/bumpInputs.nix
@@ -1,15 +1,14 @@
 # flake module that adds commands to quickly bump and commit frequently changing inputs
 # WARN: Experimental, use at your own risk
 
-# self is only used to double-check inputs
-{ withSystem, self, lib, flake-parts-lib, ... }:
+{ lib, flake-parts-lib, ... }:
 let
   inherit (flake-parts-lib) mkPerSystemOption;
   inherit (lib) mkOption types;
 in
 
 {
-  options.perSystem = mkPerSystemOption (_: {
+  options.perSystem = mkPerSystemOption ({ config, pkgs, ... }: {
     options.changingInputs = mkOption {
       description = ''
         List of names of quickly changing inputs.
@@ -19,31 +18,29 @@ in
       type = types.listOf types.str;
       default = [ ];
     };
+    config.devshells.default =
+      let
+        bumpScript = pkgs.writeShellApplication {
+          name = "bump-input";
+          runtimeInputs = [ ];
+          text = builtins.readFile ./assets/bump-input.sh;
+        };
+      in
+      {
+        commands =
+          map
+            (inputName: {
+              help =
+                # NOTE: this would somehow need access to outer args of the flake. self is not passed to the perSystem
 
+                # Double-check that the input actually exists
+                # assert builtins.elem inputName (builtins.attrNames self.inputs);
+                "Bump input ${inputName}";
+              name = "flake-bump-${inputName}";
+              command = /* bash */ "${pkgs.lib.getExe bumpScript} ${inputName}";
+              category = "flake management";
+            })
+            config.changingInputs;
+      };
   });
-  config.perSystem = { system, ... }: {
-    devshells.default = withSystem system
-      ({ pkgs, config, ... }:
-        let
-          bumpScript = pkgs.writeShellApplication {
-            name = "bump-input";
-            runtimeInputs = [ ];
-            text = builtins.readFile ./assets/bump-input.sh;
-          };
-        in
-        {
-          commands =
-            map
-              (inputName: {
-                help =
-                  # Double-check that the input actually exists
-                  assert builtins.elem inputName (builtins.attrNames self.inputs);
-                  "Bump input ${inputName}";
-                name = "flake-bump-${inputName}";
-                command = /* bash */ "${pkgs.lib.getExe bumpScript} ${inputName}";
-                category = "flake management";
-              })
-              config.changingInputs;
-        });
-  };
 }
