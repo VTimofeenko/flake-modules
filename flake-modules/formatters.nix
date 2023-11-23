a1322aa24bea14c1a8f3d9a22c3ac66c9bf91089
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-23 13:46:06 -0800

    feat: add option to add linters to the shell

--- flake-modules/formatters.nix
+++ flake-modules/formatters.nix
@@ -1,83 +1,90 @@
 # This file configures treefmt
 # By default -- only configures nixpkgs-fmt as my go-to nix formatter
-# TODO: Add option to add languages to devshell
 { flake-parts-lib
 , lib
 , ...
 }:
 let
   inherit (flake-parts-lib) mkPerSystemOption;
-  inherit (lib) mkOption types;
+  inherit (lib) mkEnableOption mkOption types;
 in
 _:
 {
-  options.perSystem = mkPerSystemOption ({ config, ... }: {
-    options.formatters = mkOption {
-      type = with types; (listOf (enum [
-        # "python" # TODO: test
-        # "config"
-        "nickel"
-        # "go"
-        # "hcl" # TODO: test
-        # "terraform" # TODO: test
-        "rust"
-        # "markdown" # TODO: think
-        "shell"
-        "scala"
-        "lua"
-        # yaml # TODO: maybe?
-        # prettier # TODO: needed?
-      ]));
-      description = "Which langs to pull in";
-      default = [ ];
-    };
-    config.treefmt = {
-      programs =
-        let
-          # enableIf :: str -> { enable: bool }
-          #  
-          # Returns a treefmt configuration attrset for a given option 
-          # if the supplied string is present in the options.formatters
-          enableIf = optionName: { enable = builtins.elem optionName config.formatters; };
-        in
-        {
-          nixpkgs-fmt.enable = true; # Always on
-
-          nickel = enableIf "nickel";
-
-          rustfmt = enableIf "rust";
-
-          shellcheck = enableIf "shell";
-
-          # black = enableIf "python";
-          # ruff = enableIf "python";
-          # isort = enableIf "python";
-
-          # dprint = enableIf "config"
-
-          # gofmt = enableIf "go"
-
-          # hclfmt = enableIf "hcl"
-          # terraform = enableIf "hcl"
-
-          # mdformat = enableIf "markdown"
-
-          scalafmt = enableIf "scala";
-
-          # deadnix.enable # maybe, dup
-          # statix.enable # Maybe, dup
-
-          stylua = enableIf "lua";
-
-          # yamlfmt = enableIf "yaml";
-
-          # prettier = enableIf "prettier"
+  options.perSystem = mkPerSystemOption ({ config, ... }:
+    let
+      cfg = config.format-module;
+    in
+    {
+      options.format-module = {
+        languages = mkOption {
+          type = with types; (listOf (enum [
+            # "python" # TODO: test
+            # "config"
+            "nickel"
+            # "go"
+            # "hcl" # TODO: test
+            # "terraform" # TODO: test
+            "rust"
+            # "markdown" # TODO: think
+            "shell"
+            "scala"
+            "lua"
+            # yaml # TODO: maybe?
+            # prettier # TODO: needed?
+          ]));
+          description = "Which langs to pull in";
+          default = [ ];
         };
-      projectRootFile = "flake.nix";
-    };
-    config.devshells.default.packages = [
-      config.treefmt.build.wrapper # Pull in pre-configured treefmt
-    ];
+        addFormattersToDevshell = mkEnableOption "if set -- adds the languages to the devshell programs";
+      };
+      config.treefmt = {
+        programs =
+          let
+            # enableIf :: str -> { enable: bool }
+            #  
+            # Returns a treefmt configuration attrset for a given option 
+            # if the supplied string is present in the options.formatters
+            enableIf = optionName: { enable = builtins.elem optionName cfg.languages; };
+          in
+          {
+            nixpkgs-fmt.enable = true; # Always on
 
-  });
+            nickel = enableIf "nickel";
+
+            rustfmt = enableIf "rust";
+
+            shellcheck = enableIf "shell";
+
+            # black = enableIf "python";
+            # ruff = enableIf "python";
+            # isort = enableIf "python";
+
+            # dprint = enableIf "config"
+
+            # gofmt = enableIf "go"
+
+            # hclfmt = enableIf "hcl"
+            # terraform = enableIf "hcl"
+
+            # mdformat = enableIf "markdown"
+
+            scalafmt = enableIf "scala";
+
+            # deadnix.enable # maybe, dup
+            # statix.enable # Maybe, dup
+
+            stylua = enableIf "lua";
+
+            # yamlfmt = enableIf "yaml";
+
+            # prettier = enableIf "prettier"
+          };
+        projectRootFile = "flake.nix";
+      };
+      config.devshells.default.packages = [
+        config.treefmt.build.wrapper # Pull in pre-configured treefmt
+      ]
+      ++ (if cfg.addFormattersToDevshell then (builtins.attrValues config.treefmt.build.programs) else [ ]);
+
+    });
 }
