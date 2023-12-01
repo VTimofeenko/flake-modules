65f89cba5e549f0cc304c9ab640372d8d59d972f
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-30 18:34:31 -0800

    break: feat: add bump all inputs option
    This commit moves bumpInputs options into a dedicated bumpInputs attrset

--- flake-modules/assets/bump-input.sh
+++ flake-modules/assets/bump-input.sh
@@ -1,7 +1,12 @@
-INPUTNAME=${1}
+INPUTNAME=${1:-}
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-echo "⬆️ Bumping input ${INPUTNAME}"
+if [[ -z $INPUTNAME ]]; then
+    echo "⬆️ Bumping all inputs"
+else
+    echo "⬆️ Bumping input ${INPUTNAME}"
+fi
+
 if [[ $(cd "$GIT_ROOT" && git status --porcelain flake.lock) ]]; then
     echo "❌flake.lock has uncommitted changes. Commit before proceeding"
     exit 1
@@ -13,11 +18,19 @@ else
     fi
     echo "❄️ Nix flake output:"
     echo "----"
-    nix flake lock --update-input "${INPUTNAME}"
+    if [[ -z $INPUTNAME ]]; then
+        nix flake update
+    else
+        nix flake lock --update-input "${INPUTNAME}"
+    fi
     echo "----"
 
     echo " Adding a commit"
-    (cd "$GIT_ROOT" && git commit --no-verify flake.lock -m "[ci]: bumping input ${INPUTNAME}") # no-verify prevents pre-commit hooks, not needed here
+    if [[ -z $INPUTNAME ]]; then
+        (cd "$GIT_ROOT" && git commit --no-verify flake.lock -m "[ci]: bumping inputs") # no-verify prevents pre-commit hooks, not needed here
+    else
+        (cd "$GIT_ROOT" && git commit --no-verify flake.lock -m "[ci]: bumping input ${INPUTNAME}") # no-verify prevents pre-commit hooks, not needed here
+    fi
 fi
 
 echo "✅All done!"
--- flake-modules/bumpInputs.nix
+++ flake-modules/bumpInputs.nix
@@ -1,46 +1,58 @@
 # flake module that adds commands to quickly bump and commit frequently changing inputs
 # WARN: Experimental, use at your own risk
+# TODO: add "bump all inputs and commit" command
 
 { lib, flake-parts-lib, ... }:
 let
   inherit (flake-parts-lib) mkPerSystemOption;
-  inherit (lib) mkOption types;
+  inherit (lib) mkOption types mkEnableOption;
 in
 
 {
-  options.perSystem = mkPerSystemOption ({ config, pkgs, ... }: {
-    options.changingInputs = mkOption {
-      description = ''
-        List of names of quickly changing inputs.
+  options.perSystem = mkPerSystemOption ({ config, pkgs, ... }:
+    let
+      cfg = config.bumpInputs;
+    in
+    {
+      options.bumpInputs.changingInputs = mkOption {
+        description = ''
+          List of names of quickly changing inputs.
 
-        This list will be turned into shell commands through devshell.
-      '';
-      type = types.listOf types.str;
-      default = [ ];
-    };
-    config.devshells.default =
-      let
-        bumpScript = pkgs.writeShellApplication {
-          name = "bump-input";
-          runtimeInputs = [ ];
-          text = builtins.readFile ./assets/bump-input.sh;
-        };
-      in
-      {
-        commands =
-          map
-            (inputName: {
-              help =
-                # NOTE: this would somehow need access to outer args of the flake. self is not passed to the perSystem
-
-                # Double-check that the input actually exists
-                # assert builtins.elem inputName (builtins.attrNames self.inputs);
-                "Bump input ${inputName}";
-              name = "flake-bump-${inputName}";
-              command = /* bash */ "${pkgs.lib.getExe bumpScript} ${inputName}";
-              category = "flake management";
-            })
-            config.changingInputs;
+          This list will be turned into shell commands through devshell.
+        '';
+        type = types.listOf types.str;
+        default = [ ];
       };
-  });
+      options.bumpInputs.bumpAllInputs = mkEnableOption "command to bump all inputs and commit";
+      config.devshells.default =
+        let
+          bumpScript = pkgs.writeShellApplication {
+            name = "bump-input";
+            runtimeInputs = [ ];
+            text = builtins.readFile ./assets/bump-input.sh;
+          };
+        in
+        {
+          commands =
+            (map
+              (inputName: {
+                help =
+                  # NOTE: this would somehow need access to outer args of the flake. self is not passed to the perSystem
+
+                  # Double-check that the input actually exists
+                  # assert builtins.elem inputName (builtins.attrNames self.inputs);
+                  "Bump input ${inputName}";
+                name = "flake-bump-${inputName}";
+                command = /* bash */ "${pkgs.lib.getExe bumpScript} ${inputName}";
+                category = "flake management";
+              })
+              cfg.changingInputs)
+            ++ lib.optional cfg.bumpAllInputs {
+              help = "Bump all inputs";
+              name = "flake-bump-all-inputs";
+              command = /* bash */ "${pkgs.lib.getExe bumpScript}";
+              category = "flake management";
+            };
+        };
+    });
 }
