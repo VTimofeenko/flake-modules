a5cf82ad04b60f3097819eb7759fda6f96286060
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-23 09:45:53 -0700

    emacs-autofmt: use more broken original file

--- flake-modules/elispAutofmt/check/target.el
+++ flake-modules/elispAutofmt/check/target.el
@@ -1,5 +1,4 @@
-(defun
-  mark-word (&optional arg allow-extend)
+(defun mark-word (&optional arg allow-extend)
   "Set mark ARG words away from point.
 The place mark goes is the same place \\[forward-word] would
 move to with the same argument.
@@ -8,8 +7,22 @@ or (in Transient Mark mode) if the mark is active,
 it marks the next ARG words after the ones already marked."
   (interactive "P\np")
   (cond
-    ((and allow-extend (or (and (eq last-command this-command) (mark t)) (region-active-p)))
-      (setq arg (if arg (prefix-numeric-value arg) (if (< (mark) (point)) -1 1)))
-      (set-mark (save-excursion (goto-char (mark)) (forward-word arg) (point))))
-    (t
-      (push-mark (save-excursion (forward-word (prefix-numeric-value arg)) (point)) nil t))))
+   ((and allow-extend
+         (or (and (eq last-command this-command) (mark t)) (region-active-p)))
+    (setq arg
+          (if arg
+              (prefix-numeric-value arg)
+            (if (< (mark) (point))
+                -1
+              1)))
+    (set-mark
+     (save-excursion
+       (goto-char (mark))
+       (forward-word arg)
+       (point))))
+   (t
+    (push-mark
+     (save-excursion
+       (forward-word (prefix-numeric-value arg))
+       (point))
+     nil t))))
