dc26499c8027753820ad8b5b5aeae4d11de2307b
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-23 11:57:37 -0700

    emacs-autofmt: correct overrides usage to fix formatting

--- flake-modules/elispAutofmt/default.nix
+++ flake-modules/elispAutofmt/default.nix
@@ -21,7 +21,11 @@ _: {
             name = "emacs-elisp-autofmt";
             runtimeInputs = [ pkgs.python3 ];
             text = ''
-              python3 ${src}/elisp-autofmt.py "$@"
+              python3 ${src}/elisp-autofmt.py \
+                --fmt-defs ${src}/elisp-autofmt.overrides.json \
+                --fmt-style native \
+                --fmt-fill-column 80 \
+                "$@"
             '';
           };
         }
@@ -40,7 +44,7 @@ _: {
               }
               ''
                 set -x
-                ORIG_MD5=$(emacs-elisp-autofmt --fmt-style fixed --stdout $src/check/orig.el | md5sum | cut -d ' ' -f 1)
+                ORIG_MD5=$(emacs-elisp-autofmt --stdout $src/check/orig.el | md5sum | cut -d ' ' -f 1)
                 TARGET_MD5=$(md5sum $src/check/target.el | cut -d ' ' -f 1)
                 if [ "$ORIG_MD5" != "$TARGET_MD5" ]; then
                   echo "md5sum does not match"
