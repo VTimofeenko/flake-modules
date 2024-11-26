0a0c34f333b293a8400ff5455646fe720ca8fef9
Vladimir Timofeenko <id@vtimofeenko.com>
2024-11-26 11:53:09 -0800

    refactor(devshell-cmds)!: enable CI commands <> config.devshellCmds.ci.enable

--- flake-modules/devShellCmds/default.nix
+++ flake-modules/devShellCmds/default.nix
@@ -32,40 +32,46 @@ in
           };
           # generateDeployNodes = mkEnableOption "Auto-populate flake.deploy.nodes output";
         };
+
+        ci.enable = mkEnableOption "ci devshell commands";
+
         appCmds = mkEnableOption "Add the flake's apps as devshell commands";
       };
 
-      config.devshells.default = {
-        env = [ ];
-        commands = # allCommands { inherit pkgs system; inherit (config.deployCmds) useDeployRs; };
-          let
-            mkDeployCmds = import ./deploy.nix {
-              deploymentConfig = config.devshellCmds.deployment;
-              inherit
-                deploy-rs
-                pkgs
-                lib
-                system
-                self
-                ;
-            };
-            ciCmds = import ./ci.nix { inherit pkgs lib; };
-          in
-          mkDeployCmds
-          ++ ciCmds
-          ++ (
-            if config.devshellCmds.appCmds then
-              (map (app: {
-                name = app;
-                command = "(cd $PRJ_ROOT && nix run .#${app})";
-                help = "Run this flake's app '${app}'";
-                category = "flake apps";
-              }) (builtins.attrNames self'.apps))
-            else
-              [ ]
-          );
-        packages = [ ];
-      };
+      config.devshells.default =
+        let
+          cfg = config.devshellCmds;
+        in
+        {
+          env = [ ];
+          commands = # allCommands { inherit pkgs system; inherit (config.deployCmds) useDeployRs; };
+            let
+              mkDeployCmds = import ./deploy.nix {
+                deploymentConfig = config.devshellCmds.deployment;
+                inherit
+                  deploy-rs
+                  pkgs
+                  lib
+                  system
+                  self
+                  ;
+              };
+            in
+            mkDeployCmds
+            ++ (lib.optionals cfg.ci.enable (import ./ci.nix { inherit pkgs lib; }))
+            ++ (
+              if config.devshellCmds.appCmds then
+                (map (app: {
+                  name = app;
+                  command = "(cd $PRJ_ROOT && nix run .#${app})";
+                  help = "Run this flake's app '${app}'";
+                  category = "flake apps";
+                }) (builtins.attrNames self'.apps))
+              else
+                [ ]
+            );
+          packages = [ ];
+        };
     }
   );
   # TODO: auto-generate flake.deploy.nodes output
