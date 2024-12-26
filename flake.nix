5e3eae90c6a3083ae392659fc5c18e342045600b
Vladimir Timofeenko <id@vtimofeenko.com>
2024-12-25 18:03:24 -0800

    refactor: 24.05 -> 24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.follows = "nixpkgs-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
